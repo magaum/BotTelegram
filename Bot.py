ee61ed6cfe4d23db94925da278a23fcf9832db1c
weslei_luiz22 <weslei_luiz22@hotmail.com>
2017-09-08 21:19:48 -0400

    Criacao de classes e metodos: 1 - Classe Main 2 - Classe Bot
    Problemas foram encontrados na leitura de caracteres especiais ao concatenar no telegram #Bug
    Botoes foram criados
    Formatacao de texto esta feita

---
+++ Bot.py
@@ -0,0 +1,41 @@
+import telepot
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+
+#atribuindo token ao Bot
+bot = telepot.Bot("444778927:AAEIM7sIL9PM5hfl-VtqQAQaqj2-BwujQ68")
+
+class Bot():
+  def init():
+#inicia o programa e deixa em execução
+    while True:
+      pass
+     
+  def mensagem(msg):
+    #atribuindo ultimas atualizações feitas por usuários ("mensagens encaminhadas para o bot")
+      updates = bot.getUpdates(-1)
+    #atribuindo Id do usuário a uma variável
+      chatId = updates[0]['message']['chat']['id']
+    #atribuindo nome de usuário a uma variável
+      username = updates[0]['message']['chat']['username']
+    #mandando mensagem de boas vindas ao usuário
+      if(msg['text'] == "/start"):
+        bot.sendMessage(chatId,'''Olá, %s sou o FAQtec\n     
+ Posso esclarecer dúvidas frequentes referente a faculdade\n
+ Para escolher alguma opção selecione um botão''' %username,
+      reply_markup=ReplyKeyboardMarkup(
+          keyboard=[
+            [KeyboardButton (text="Como ser aluno"),KeyboardButton (text="Cursos"),KeyboardButton (text="Professores")]
+                   ]
+      ))
+
+      if(msg['text'] == 'Cursos'):
+#Abre o arquivo Help.html com o atributo leitura
+        txtHelp = open('Help.html','r')
+#Envia mensagem com o conteúdo do arquivo Help.txt
+        bot.sendMessage(chatId,txtHelp.read(),'Markdown')
+#Fecha o arquivo
+        txtHelp.close()
+
+#Quando alguma mensagem é enviada do telegram
+#O metodo message_loop chama a função mensagem passando o que foi enviado do telegram como parametro para função        
+  bot.message_loop(mensagem)
