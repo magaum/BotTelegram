d907ca2c2bdd9b3c68c3dd543f8ca5bd405aaebf
magaum <weslei_luiz22@hotmail.com>
2017-10-25 10:52:26 -0200

    botTelegram
    Tratativa para usuários que não possuem username

--- Bot.py
+++ Bot.py
@@ -5,21 +5,27 @@ from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 bot = telepot.Bot("444778927:AAEIM7sIL9PM5hfl-VtqQAQaqj2-BwujQ68")
 
 class Bot():
-  def init():
+	def init():
 #inicia o programa e deixa em execução
-    while True:
-      pass
+		while True:
+			pass
      
-  def mensagem(msg):
+	def mensagem(msg):
     #atribuindo ultimas atualizações feitas por usuários ("mensagens encaminhadas para o bot")
-      updates = bot.getUpdates(-1)
+		updates = bot.getUpdates(-1)
     #atribuindo Id do usuário a uma variável
-      chatId = updates[0]['message']['chat']['id']
-    #atribuindo nome de usuário a uma variável
-      username = updates[0]['message']['chat']['username']
+		chatId = updates[0]['message']['chat']['id']
+	#atribuindo nome de usuário a uma variável
+		try:
+			username = updates[0]['message']['chat']['username']
+		except KeyError:
+			username = 'true'
+	#tratativa para usuários que não possuem username
+		if (type (username) != 'true'):
+			username = 'amigo'
     #mandando mensagem de boas vindas ao usuário
-      if(msg['text'] == "/start"):
-        bot.sendMessage(chatId,'''Olá, %s sou o FAQtec\n     
+		if(msg['text'] == "/start"):
+			bot.sendMessage(chatId,'''Olá, %s sou o FAQtec\n     
  Posso esclarecer dúvidas frequentes referente a faculdade\n
  Para escolher alguma opção selecione um botão''' %username,
       reply_markup=ReplyKeyboardMarkup(
@@ -27,18 +33,18 @@ class Bot():
             [KeyboardButton (text="Como ser aluno"),KeyboardButton (text="Cursos"),KeyboardButton (text="Professores")]
                    ]
       ))
-
-      if(msg['text'] == 'Cursos'):
+	  
+		if(msg['text'] == 'Cursos'):
 #Abre o arquivo Hello.md com o atributo leitura
-        txtHelp = open('Hello.md','r')
+			txtHelp = open('Hello.md','r')
   
   #Ler arquivos de outras pasta EX.: ComoSerAluno/Vestibular.md
   
 #Envia mensagem com o conteúdo do arquivo Help.txt
-        bot.sendMessage(chatId,txtHelp.read(),'Markdown')
+		bot.sendMessage(chatId,txtHelp.read(),'Markdown')
 #Fecha o arquivo
-        txtHelp.close()
+		txtHelp.close()
 
 #Quando alguma mensagem é enviada do telegram
 #O metodo message_loop chama a função mensagem passando o que foi enviado do telegram como parametro para função        
-  bot.message_loop(mensagem)
+	bot.message_loop(mensagem)
